ff40c38a34df557524563d94baa5a9327c695f07
DarienAllison1 <124842514+DarienAllison1@users.noreply.github.com>
2024-11-27 12:04:51 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,14 +1,23 @@
+import java.util.Random;
+
 /**
  * Main class to demonstrate the functionality of the Node class.
  */
 public class Main {
     public static void main(String[] args) {
-        Node root = new Node("Root", 10);
-        root.insert(5);
-        root.insert(15);
-        root.insert(3);
-        root.insert(7);
+        Random random = new Random();
+        
+        // Generate a random root value between 1 and 100
+        int rootValue = random.nextInt(100) + 1;
+        Node root = new Node("Root", rootValue);
+
+        // Insert random values into the tree
+        for (int i = 0; i < 4; i++) {
+            int randomValue = random.nextInt(100) + 1;
+            root.insert(randomValue);
+        }
 
         System.out.println(root.dump(0));
     }
 }
+
